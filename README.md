5a2f6e132e487f070be104db4e6ceaa6390b2347
Eder-martinez <122801090+Eder-martinez@users.noreply.github.com>
2023-02-04 02:44:19 +0000

    numero mayor

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 |No.|Arichivo|Descripcion|
 |--|--|--|
 |1|prueba1.py|suma de dos numeros|
+|2|prueba2.py|numero mayor es: |
---
+++ semana1/prueba2.py
@@ -0,0 +1,7 @@
+numero1 = int(input("escriba un numero: "))
+numero2 = int(input("escriba otro numero: "))
+if numero1 >= numero2 : 
+    print("El numero mayor es: {}".format(numero1))
+else:
+    print("El numero mayor es: {}".format(numero2))
+    
